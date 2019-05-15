10022eac5dc3b1600721d4b49e159766addf09b8
Hidde Braun <hidde.braun@gmail.com>
2019-05-15 22:22:46 +0200

    Juiste pad naar public folder in voorbeeld config bestand.

--- private/includes/config.example.php
+++ private/includes/config.example.php
@@ -7,7 +7,7 @@ $config = [
     'DB_PASSWORD' => 'root',
     'ROOT' => dirname(dirname(__DIR__)),
     'PRIVATE' => dirname(__DIR__),
-    'WEBROOT' => dirname(__DIR__) . '/public',
+    'WEBROOT' => dirname(dirname(__DIR__)) . '/public'
 ];
 
 return $config;
